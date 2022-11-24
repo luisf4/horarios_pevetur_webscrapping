9301032e08a5017d8d658e5be764174653b4e33f
luis <luisf886222@gmail.com>
2022-11-24 09:54:10 -0300

    alterações no script

--- treco.py
+++ treco.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import docx
 # import aspose.words as aw
 
@@ -7,6 +8,24 @@ import docx
 doc = docx.Document('out.docx')
 
 lista_cidade = [
+   'S.J.RPRETOÀBADYBASSITTR$5,30TempoMédiodoPercurso:50minutos'
+   'BADYBASSITTÀSJRIOPRETO',
+   'SJRIOPRETOÀNOVAALIANÇAR$6,30TempoMédiodoPercurso:1Hora',
+   'NOVAALIANÇAÀSJRIOPRETO',
+   'SJRIOPRETOÀNOVAITAPIREMAR$7,50TempoMédiodoPercurso:1Horae15minutos',
+   'NOVAITAPIREMAÀSJRIOPRETO',
+   'SJRIOPRETOÀMENDONÇAR$10,35TempoMédiodoPercurso:1Horae30minutos',
+   'MENDONÇAÀSJRIOPRETO',
+   'SJRIOPRETOÀADOLFOR$12,25TempoMédiodoPercurso:1Horae45minutos',
+   'ADOLFOÀSJRIOPRETO',
+   'SJRIOPRETOÀMIRASSOLÂNDIAR$6,55TempoMédiodoPercurso:1Hora',
+   'MIRASSOLÂNDIAÀSJRIOPRETO',
+   'SJRIOPRETOÀIPIGUÁR$5,30TempoMédiodoPercurso:45minutos',
+   'IPIGUÁÀSJRIOPRETO',
+   'SJRIOPRETOÀRECANTO18R$5,30TempoMédiodoPercurso:20minutos',
+   'RECANTO18ÀSJRIOPRETO',
+   'SJRIOPRETOÀMACAÚBASR$7,50TempoMédiodoPercurso:1Horae15Minutos',
+   'MACAÚBASÀSJRIOPRETO',
    
 ]
 
@@ -19,6 +38,13 @@ lista_dias = [
    'SÁBADOS',
 ]
 
+# Variaveis 
+index = 0
+cidade = False
+sabado = False
+domingo = False
+id = 999
+
 
 for i in doc.paragraphs:
 
@@ -26,10 +52,10 @@ for i in doc.paragraphs:
    linha= " ".join((i.text).strip().replace(" ","").split())
 
    if linha != '':
-      if linha in lista_dias:
-         pass
-      else: 
-         print(linha)
+      if linha in lista_cidade:
+         if linha == lista_cidade[0]:
+            cidade = True
+            id = 0
 
 
    else:
